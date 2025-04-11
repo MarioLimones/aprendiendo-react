87d1f6e79d88257b6d5df2fd72e63d2639505b5a
MarioLimones <mario.limobe@gmail.com>
2025-04-12 00:36:04 +0200

    Primer commit - Proyecto React inicial

---
+++ aprendiendo-react/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+export function App () {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/google/netflix.com" alt="Foto Netflix" />
+            </header>
+            <div>
+                <strong>Pablo Mallado Troyano</strong>
+                <span> @pablitomalladito</span>
+            </div>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
